b85ab2a82d2d5781b6d5fdd553040397bfcec842
afrivera <andresrfelip@hotmail.com>
2022-09-18 20:56:37 -0500

    ejemplo consumidor productor con wait y notify

---
+++ src/org/afrivera/hilos/ejemplosync/EjemploProductorConsumidorJava8.java
@@ -0,0 +1,28 @@
+package org.afrivera.hilos.ejemplosync;
+
+import org.afrivera.hilos.ejemplosync.runnable.Consumidor;
+import org.afrivera.hilos.ejemplosync.runnable.Panadero;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class EjemploProductorConsumidorJava8 {
+    public static void main(String[] args) {
+        Panaderia p = new Panaderia();
+        new Thread(()->{
+            for (int i = 0; i < 10; i++) {
+                p.hornear("Pan n°: " + i);
+                try {
+                    Thread.sleep(ThreadLocalRandom.current().nextInt(500, 2000));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        new Thread(()->{
+            for (int i = 0; i < 10; i++) {
+                p.consumir();
+            }
+        }).start();
+        
+    }
+}
